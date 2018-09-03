4dc441cca72fcefc816ad05f9e3f23acb2c6cc06
mikhawa <michaeljpitz@gmail.com>
2018-09-03 13:36:53 +0200

    03-ok

--- 03-transformation-tableau-menu.php
+++ 03-transformation-tableau-menu.php
@@ -3,9 +3,10 @@
  * création d'un menu à partir d'un tableau
  */
 
-class Menu{
+class Menu
+{
     // Attributs
-    private $sortie ="";
+    private $sortie = "";
     private $startLine = "<ul>";
     private $startRow = "<li>";
     private $endLine = "</ul>";
@@ -18,26 +19,37 @@ class Menu{
     }
 
     // private method
-    private function createMenu(array $tabs){
+    private function createMenu(array $tabs)
+    {
+        // first ul
         $this->setSortie($this->startLine);
-        foreach ($tabs as $key => $values){
-            // if the value is a array
-            if(is_array($values)){
 
-            }else{
-                $this->setSortie($this->startRow.$values.$this->endRow);
+        // loops
+        foreach ($tabs as $key => $values) {
+            // if the value is a array
+            if (is_array($values)) {
+
+                // recursive
+                $this->createMenu($values);
+
+            } else {
+                // item <li></li>
+                $this->setSortie($this->startRow . $values . $this->endRow);
             }
         }
+        // last close ul
         $this->setSortie($this->endLine);
     }
 
     // public getter
-    public function getSortie(){
+    public function getSortie()
+    {
         return $this->sortie;
     }
 
     // public setter
-    public function setSortie(string $str){
+    public function setSortie(string $str)
+    {
         // concatenation of sortie
         $this->sortie .= $str;
     }
@@ -46,11 +58,17 @@ class Menu{
 
 $data = [
     "un",
+    ["enfant de un"],
     "deux",
     [
         "trois",
+        [
+                "quatre",
+                "cinq",
+                ["six","sept"],
+            ],
     ],
-    "quatre",
+    "huit",
 ];
 
 $pourmenu = new Menu($data);
@@ -62,8 +80,10 @@ $menu = $pourmenu->getSortie();
     <title>Notre menu</title>
 </head>
 <body>
-    <h1>Menu</h1>
-<div id="menu"><?=$menu?></div>
+<h1>Menu</h1>
+<div id="menu">
+    <nav><?= $menu ?></nav>
+</div>
 <div id="content"></div>
 </body>
 </html>
